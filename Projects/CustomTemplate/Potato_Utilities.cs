56ed9f950bc3caa13cc213b652dbda206bd7aff6
potatiumstotalis <83508111+potatiumstotalis@users.noreply.github.com>
2023-09-02 23:28:15 +0100

    0.0.0.2

--- Projects/CustomTemplate/Potato_Utilities.cs
+++ Projects/CustomTemplate/Potato_Utilities.cs
@@ -17,7 +17,8 @@ using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ObjectBuilders.Definitions;
 using VRageMath;
-using static IngameScript.Program.Potato_Utilities.Easing;
+using static IngameScript.Program.Potato_Utilities.Animation;
+using static IngameScript.Program.Potato_Utilities.Sequence;
 
 namespace IngameScript
 {
@@ -25,54 +26,84 @@ namespace IngameScript
     {
         public class Potato_Utilities
         {
-            public static class Easing
+
+            public class Animation
             {
+                public enum AnimationType
+                {
+                    time,
+                    distance
+                }
                 public enum EasingType
                 {
-                    Linear,
-                    Quadratic,
-                    Cubic,
-                    Quartic,
-                    Quintic,
-                    Sine
+                    linear,
+                    quadratic,
+                    cubic,
+                    quartic,
+                    quintic,
+                    sine
                 }
-
                 public enum EasingDirection
                 {
-                    In,
-                    Out
+                    ezin,
+                    ezout
                 }
+
+                public double Animate(AnimationType animationType, EasingType easingType, EasingDirection easingDirection, double startValue, double endValue, double variable1, double variable2)
+                {
+
+                    switch (animationType)
+                    {
+                        case AnimationType.time:
+                            return Potato_Utilities.Animation.AnimateTime(startValue, endValue, variable1, variable2, type, direction);
+                        case AnimationType.distance:
+                            return Potato_Utilities.Animation.AnimateDistance(startValue, endValue, variable1, variable2, type, direction);
+                        default:
+                            throw new ArgumentException("Invalid animation type");
+                    }
+                }
+                public static double AnimateTime(double startValue, double endValue, double currentTime, double duration, EasingType easingType, EasingDirection easingDirection)
+                {
+                    double change = endValue - startValue;
+                    return Easing.Ease(easingType, easingDirection, currentTime, startValue, change, duration);
+                }
+                public static double AnimateDistance(double startValue, double endValue, double currentPosition, double targetPosition, EasingType easingType, EasingDirection easingDirection)
+                {
+                    double ratio = (currentPosition - startValue) / (targetPosition - startValue);
+                    double value = startValue + ratio * (endValue - startValue);
+                    return Easing.Ease(easingType, easingDirection, value, startValue, endValue - startValue, 1);
+                }
+
+
 
                 public static double Ease(EasingType type, EasingDirection direction, double t, double b, double c, double d)
                 {
                     switch (type)
                     {
-                        case EasingType.Linear:
+                        case EasingType.linear:
                             return LinearEase(direction, t, b, c, d);
-                        case EasingType.Quadratic:
+                        case EasingType.quadratic:
                             return QuadraticEase(direction, t, b, c, d);
-                        case EasingType.Cubic:
+                        case EasingType.cubic:
                             return CubicEase(direction, t, b, c, d);
-                        case EasingType.Quartic:
+                        case EasingType.quartic:
                             return QuarticEase(direction, t, b, c, d);
-                        case EasingType.Quintic:
+                        case EasingType.quintic:
                             return QuinticEase(direction, t, b, c, d);
-                        case EasingType.Sine:
+                        case EasingType.sine:
                             return SineEase(direction, t, b, c, d);
                         default:
                             throw new ArgumentException("Invalid easing type");
                     }
                 }
-
                 private static double LinearEase(EasingDirection direction, double t, double b, double c, double d)
                 {
                     return c * t / d + b;
                 }
-
                 private static double QuadraticEase(EasingDirection direction, double t, double b, double c, double d)
                 {
                     t /= d;
-                    if (direction == EasingDirection.In)
+                    if (direction == EasingDirection.ezin)
                     {
                         return c * t * t + b;
                     }
@@ -81,11 +112,10 @@ namespace IngameScript
                         return -c * t * (t - 2) + b;
                     }
                 }
-
                 private static double CubicEase(EasingDirection direction, double t, double b, double c, double d)
                 {
                     t /= d;
-                    if (direction == EasingDirection.In)
+                    if (direction == EasingDirection.ezin)
                     {
                         return c * t * t * t + b;
                     }
@@ -95,11 +125,10 @@ namespace IngameScript
                         return c * (t * t * t + 1) + b;
                     }
                 }
-
                 private static double QuarticEase(EasingDirection direction, double t, double b, double c, double d)
                 {
                     t /= d;
-                    if (direction == EasingDirection.In)
+                    if (direction == EasingDirection.ezin)
                     {
                         return c * t * t * t * t + b;
                     }
@@ -109,11 +138,10 @@ namespace IngameScript
                         return -c * (t * t * t * t - 1) + b;
                     }
                 }
-
                 private static double QuinticEase(EasingDirection direction, double t, double b, double c, double d)
                 {
                     t /= d;
-                    if (direction == EasingDirection.In)
+                    if (direction == EasingDirection.ezin)
                     {
                         return c * t * t * t * t * t + b;
                     }
@@ -123,10 +151,9 @@ namespace IngameScript
                         return c * (t * t * t * t * t + 1) + b;
                     }
                 }
-
                 private static double SineEase(EasingDirection direction, double t, double b, double c, double d)
                 {
-                    if (direction == EasingDirection.In)
+                    if (direction == EasingDirection.ezin)
                     {
                         return -c * Math.Cos(t / d * (Math.PI / 2)) + c + b;
                     }
@@ -136,41 +163,23 @@ namespace IngameScript
                     }
                 }
             }
-            public class Animation
-            {
-                // Animate any variable through time
-                public static double AnimateTime(double startValue, double endValue, double currentTime, double duration, EasingType easingType, EasingDirection easingDirection)
-                {
-                    // Implementation here
-                }
 
-                // Animate any variable through distance
-                public static double AnimateDistance(double startValue, double endValue, double currentPosition, double targetPosition)
-                {
-                    // Implementation here
-                }
-
-                // Apply smoothing (easing)
-                public static double ApplySmoothing(double value, EasingType easingType, EasingDirection easingDirection)
-                {
-                    // Implementation here
-                }
-            }
             public class Sequence
             {
-                // Queue to hold animations
-                private Queue<Animation> animationQueue = new Queue<Animation>();
+                private Queue<Action> animationQueue = new Queue<Action>();
 
-                // Add an animation to the sequence queue
-                public void AddToQueue(Animation animation)
+                public void AddToQueue(Action animation)
                 {
-                    // Implementation here
+                    animationQueue.Enqueue(animation);
                 }
 
-                // Execute the animations in the sequence
                 public void Execute()
                 {
-                    // Implementation here
+                    while (animationQueue.Count > 0)
+                    {
+                        Action animation = animationQueue.Dequeue();
+                        animation();
+                    }
                 }
             }
 
