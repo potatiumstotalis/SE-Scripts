07df9877a9b7d5af0135b95653a1c91d9d24eb2d
potatiumstotalis <83508111+potatiumstotalis@users.noreply.github.com>
2023-09-03 19:24:23 +0100

    0.0.2.0

--- Projects/CustomTemplate/Potato_Utilities.cs
+++ Projects/CustomTemplate/Potato_Utilities.cs
@@ -22,112 +22,163 @@ namespace IngameScript
 {
     partial class Program
     {
-        public class Potato_Utilities
+        public class PotatoAnimation
         {
-            public class Animation
+            public static class Sequence
             {
-                // Fields and methods for managing a sequence of Animation_Parts
+                //Write methods for sequencing/concatenating animations
             }
 
-            public class Animation_Part
+            //Easing: AnimateTime.Ease(value, startValue, endValue, direction, type)
+            public static class Time
             {
-                public enum PartType
-                {
-                    EaseIn,
-                    EaseOut,
-                    Hold
-                }
+                public enum Direction { In, Out, InOut }
+                public enum Type { linear, quad, cubic, quart, quint, sine, expo, circ }
 
-                public enum AnimationType
-                {
-                    time,
-                    distance
-                }
 
-                public enum EasingType
+                //Normalize from startValue-endValue to 0-1
+                public static float Normalize(float value, float startValue, float endValue)
                 {
-                    linear,
-                    sine,
-                    half,
-                    cubic
-                }
-
-                public enum EasingDirection
-                {
-                    ezin,
-                    ezout
-                }
-
-                public double Animate(AnimationType animationType, EasingType easingType, EasingDirection easingDirection, double startValue, double endValue, double variable1, double variable2)
-                {
-                    switch (animationType)
+                    // Check if startValue and endValue are the same to avoid division by zero
+                    if (startValue == endValue)
                     {
-                        case AnimationType.time:
-                            return AnimateTime(startValue, endValue, variable1, variable2, easingType, easingDirection);
-                        case AnimationType.distance:
-                            return AnimateDistance(startValue, endValue, variable1, easingType, easingDirection);
+                        throw new ArgumentException("startValue and endValue cannot be the same.");
+                    }
+
+                    // Determine the direction
+                    bool isPositiveDirection = endValue > startValue;
+
+                    // Normalize the value based on the direction
+                    float normalizedValue;
+                    if (isPositiveDirection)
+                    {
+                        normalizedValue = (value - startValue) / (endValue - startValue);
+                    }
+                    else
+                    {
+                        normalizedValue = (startValue - value) / (startValue - endValue);
+                    }
+
+                    return normalizedValue;
+                }
+
+                //Denormalize from 0-1 to startValue-endValue
+                public static float Denormalize(float normalizedValue, float startValue, float endValue)
+                {
+                    // Check if the normalizedValue is out of the 0-1 range
+                    if (normalizedValue < 0 || normalizedValue > 1)
+                    {
+                        throw new ArgumentException("normalizedValue must be between 0 and 1.");
+                    }
+
+                    // Determine the direction
+                    bool isPositiveDirection = endValue > startValue;
+
+                    // Denormalize the value based on the direction
+                    float originalValue;
+                    if (isPositiveDirection)
+                    {
+                        originalValue = startValue + normalizedValue * (endValue - startValue);
+                    }
+                    else
+                    {
+                        originalValue = startValue - normalizedValue * (startValue - endValue);
+                    }
+
+                    return originalValue;
+                }
+                
+                //Ease the Normalized value
+                public static float Ease(float t, Direction direction, Type type)
+                {
+                    switch (type)
+                    {
+                        case Type.linear:
+                            return Linear(t);
+                        case Type.quad:
+                            return direction == Direction.In ? InQuad(t) : direction == Direction.Out ? OutQuad(t) : InOutQuad(t);
+                        case Type.cubic:
+                            return direction == Direction.In ? InCubic(t) : direction == Direction.Out ? OutCubic(t) : InOutCubic(t);
+                        case Type.quart:
+                            return direction == Direction.In ? InQuart(t) : direction == Direction.Out ? OutQuart(t) : InOutQuart(t);
+                        case Type.quint:
+                            return direction == Direction.In ? InQuint(t) : direction == Direction.Out ? OutQuint(t) : InOutQuint(t);
+                        case Type.sine:
+                            return direction == Direction.In ? InSine(t) : direction == Direction.Out ? OutSine(t) : InOutSine(t);
+                        case Type.expo:
+                            return direction == Direction.In ? InExpo(t) : direction == Direction.Out ? OutExpo(t) : InOutExpo(t);
+                        case Type.circ:
+                            return direction == Direction.In ? InCirc(t) : direction == Direction.Out ? OutCirc(t) : InOutCirc(t);
                         default:
-                            throw new ArgumentException("Invalid animation type");
+                            throw new ArgumentException("Invalid easing type.");
                     }
                 }
 
-                public static double AnimateTime(double startValue, double endValue, double currentTime, double duration, EasingType easingType, EasingDirection easingDirection)
+                //Return the Final value
+                public static float Animate(float value, float startValue, float endValue, Direction dir, Type type)
                 {
-                    double difference = endValue - startValue; // Automatically handles direction
-                    return Ease(easingType, currentTime, startValue, difference, duration);
+                    float normalizedValue = Normalize(value, startValue, endValue);
+                    float easedValue = Ease(normalizedValue, dir, type);
+                    float denormalizedValue = Denormalize(easedValue, startValue, endValue);
+                    return denormalizedValue;
                 }
 
-                public static double AnimateDistance(double startValue, double endValue, double currentPosition, EasingType easingType, EasingDirection easingDirection)
+                //Easing Functions | Imported from: https://gist.github.com/Kryzarel/bba64622057f21a1d6d44879f9cd7bd4
+                public static float Linear(float t) => t;
+
+                public static float InQuad(float t) => t * t;
+                public static float OutQuad(float t) => 1 - InQuad(1 - t);
+                public static float InOutQuad(float t)
                 {
-                    double difference = endValue - startValue; // Automatically handles direction
-                    double target = Math.Abs(endValue - currentPosition);
-                    return Ease(easingType, currentPosition, startValue, difference, target);
+                    if (t < 0.5) return InQuad(t * 2) / 2;
+                    return 1 - InQuad((1 - t) * 2) / 2;
                 }
 
-
-
-                // Easing Variable Explanations:
-                /* 
-                 * c: Current Time/Position — The current time or position of the animation.
-                 * s: Starting Value — The initial value of the property being animated.
-                 * d: Difference — The change in the property value, calculated as the ending value minus the starting value.
-                 * t: Target Duration/Position — The total time or target position the animation will take or reach.
-                 */
-
-                public static double Ease(EasingType type, double c, double s, double d, double t)
+                public static float InCubic(float t) => t * t * t;
+                public static float OutCubic(float t) => 1 - InCubic(1 - t);
+                public static float InOutCubic(float t)
                 {
-                    return type == EasingType.linear ? LinearEase(c, s, d, t) :
-                           type == EasingType.sine ? SineEase(c, s, d, t) :
-                           type == EasingType.half ? HalfEase(c, s, d, t) :
-                           type == EasingType.cubic ? CubicEase(c, s, d, t) :
-                           0; // Default case, can also throw an exception if you prefer
+                    if (t < 0.5) return InCubic(t * 2) / 2;
+                    return 1 - InCubic((1 - t) * 2) / 2;
                 }
 
-                private static double LinearEase(double c, double s, double d, double t) => d * c / t + s;
-                private static double SineEase(double c, double s, double d, double t) => d * Math.Sin(c / t * (Math.PI / 2)) + s;
-                private static double HalfEase(double c, double s, double d, double t) => d * Math.Sqrt(c / t) + s;
-                private static double CubicEase(double c, double s, double d, double t) => d * (c /= t) * c * c + s;
+                public static float InQuart(float t) => t * t * t * t;
+                public static float OutQuart(float t) => 1 - InQuart(1 - t);
+                public static float InOutQuart(float t)
+                {
+                    if (t < 0.5) return InQuart(t * 2) / 2;
+                    return 1 - InQuart((1 - t) * 2) / 2;
+                }
 
+                public static float InQuint(float t) => t * t * t * t * t;
+                public static float OutQuint(float t) => 1 - InQuint(1 - t);
+                public static float InOutQuint(float t)
+                {
+                    if (t < 0.5) return InQuint(t * 2) / 2;
+                    return 1 - InQuint((1 - t) * 2) / 2;
+                }
+
+                public static float InSine(float t) => (float)-Math.Cos(t * Math.PI / 2);
+                public static float OutSine(float t) => (float)Math.Sin(t * Math.PI / 2);
+                public static float InOutSine(float t) => (float)(Math.Cos(t * Math.PI) - 1) / -2;
+
+                public static float InExpo(float t) => (float)Math.Pow(2, 10 * (t - 1));
+                public static float OutExpo(float t) => 1 - InExpo(1 - t);
+                public static float InOutExpo(float t)
+                {
+                    if (t < 0.5) return InExpo(t * 2) / 2;
+                    return 1 - InExpo((1 - t) * 2) / 2;
+                }
+
+                public static float InCirc(float t) => -((float)Math.Sqrt(1 - t * t) - 1);
+                public static float OutCirc(float t) => 1 - InCirc(1 - t);
+                public static float InOutCirc(float t)
+                {
+                    if (t < 0.5) return InCirc(t * 2) / 2;
+                    return 1 - InCirc((1 - t) * 2) / 2;
+                }
             }
 
-            public class Sequence
-            {
-                private Queue<Action> animationQueue = new Queue<Action>();
-
-                public void AddToQueue(Action animation)
-                {
-                    animationQueue.Enqueue(animation);
-                }
-
-                public void Execute()
-                {
-                    while (animationQueue.Count > 0)
-                    {
-                        Action animation = animationQueue.Dequeue();
-                        animation();
-                    }
-                }
-            }
         }
     }
 }
