8ea3b9fc7cdbfa3767068ee714e81d15f2d10665
potatiumstotalis <83508111+potatiumstotalis@users.noreply.github.com>
2023-09-03 20:36:51 +0100

    0.0.2.6

--- Projects/LightTest/Program.cs
+++ Projects/LightTest/Program.cs
@@ -22,27 +22,81 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
+        // Initialize self-updating
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script.
+            Runtime.UpdateFrequency = UpdateFrequency.None; // No automatic updates
         }
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means.
+            // Save state here
         }
+
+        // Default Values
+        float maxTime = 5f;
+        float targetIntensity = 2f;
+        PotatoAnimation.Time.Direction direction = PotatoAnimation.Time.Direction.InOut;
+        PotatoAnimation.Time.Type type = PotatoAnimation.Time.Type.sine;
+        float currentTime = 0f;
+        float initialIntensity;
+        bool isAnimating = false;
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from.
-        }
+            if ((updateSource & UpdateType.Terminal) != 0)
+            {
+                // Parse the argument
+                string[] args = argument.Split(',');
+                string lightName = args.Length > 0 ? args[0].Trim() : "Light1";
+                float targetIntensity = args.Length > 1 ? float.Parse(args[1].Trim()) : 2f;
+                maxTime = args.Length > 2 ? float.Parse(args[2].Trim()) : 5f;
+                PotatoAnimation.Time.Direction direction = args.Length > 3 ? (PotatoAnimation.Time.Direction)Enum.Parse(typeof(PotatoAnimation.Time.Direction), args[3].Trim()) : PotatoAnimation.Time.Direction.InOut;
+                PotatoAnimation.Time.Type type = args.Length > 4 ? (PotatoAnimation.Time.Type)Enum.Parse(typeof(PotatoAnimation.Time.Type), args[4].Trim()) : PotatoAnimation.Time.Type.sine;
 
+                // Get the light block
+                IMyInteriorLight light = GridTerminalSystem.GetBlockWithName(lightName) as IMyInteriorLight;
+
+                if (light != null)
+                {
+                    // Capture the initial intensity
+                    initialIntensity = light.Intensity;
+
+                    // Start the animation
+                    isAnimating = true;
+
+                    // Enable self-updating
+                    Runtime.UpdateFrequency = UpdateFrequency.Update1;
+                }
+            }
+
+            if (isAnimating)
+            {
+                // Get the light block again
+                IMyInteriorLight light = GridTerminalSystem.GetBlockWithName("Light1") as IMyInteriorLight;
+
+                if (light != null)
+                {
+                    // Animate the intensity
+                    float newIntensity = PotatoAnimation.Time.Animate(currentTime, maxTime, initialIntensity, targetIntensity, direction, type);
+
+                    // Update the light intensity
+                    light.Intensity = newIntensity;
+
+                    // Update the current time
+                    currentTime += 0.1f;  // Update1 is approximately 0.1 seconds
+
+                    // Reset the timer and stop animating if it reaches maxTime
+                    if (currentTime >= maxTime)
+                    {
+                        currentTime = 0f;
+                        isAnimating = false;
+
+                        // Disable self-updating
+                        Runtime.UpdateFrequency = UpdateFrequency.None;
+                    }
+                }
+            }
+        }
     }
 }
